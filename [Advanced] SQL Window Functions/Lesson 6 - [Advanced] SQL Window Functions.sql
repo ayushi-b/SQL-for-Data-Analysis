77873762fc91c24049a2068e4e3ebc075fd61365
ayushi-b <kanchibansal08@gmail.com>
2018-06-18 23:46:16 +0530

    No one told you this

--- [Advanced] SQL Window Functions/Lesson 6 - [Advanced] SQL Window Functions.sql
+++ [Advanced] SQL Window Functions/Lesson 6 - [Advanced] SQL Window Functions.sql
@@ -2,7 +2,7 @@
 Create a running total of standard_amt_usd (in the orders table) over order time with 
 no date truncation. Your final table should have two columns: one with the amount 
 being added for each new row, and a second with the running total.
-*/
+*/  
 
 SELECT standard_amt_usd,
 	SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
